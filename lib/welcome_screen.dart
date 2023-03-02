0fc9027211983a4c63e14eabf77e170bd76f78ac
Shadreck kasera <shadkasera@gmail.com>
2023-03-03 00:00:13 +0200

    modified files

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -69,7 +69,7 @@ class WelcomeScreen extends StatelessWidget {
         
         body: RichText(
                 text: const TextSpan(
-                  text: 'WELCOME TO MUST TIMETABLES! \n\n',
+                  text: 'WELCOME ! \n\n',
                   style: TextStyle(
                     fontSize: 34,
                     color: Colors.black,
